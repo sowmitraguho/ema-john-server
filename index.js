0db50d637eed83d74bb65d42b1c578ee00129678
Sowmitra Guha <sowmmochy@gmail.com>
2025-06-22 02:19:41 +0600

    variable name capitalize

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 //const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
-const uri = process.env.uri;
+const uri = process.env.URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
